0d9022a41e0fcd2a9c2943e43c7cb73d353e2f34
Ewennn <ewen.bosquet@gmail.com>
2025-05-10 14:44:08 +0200

    feat/ Prepare gateway to redirect all routes

--- main.go
+++ main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
-const navURL = "http://localhost:8081"
-
 func main() {
-	target, err := url.Parse("http://localhost:8081")
+	config, err := NewConfig()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("failed to load config", err)
 	}
 
-	navigationProxy := httputil.NewSingleHostReverseProxy(target)
+	//usersProxy := httputil.NewSingleHostReverseProxy(mustParseURL(config.UsersHost, config.UsersHost))
+	//incidentsProxy := httputil.NewSingleHostReverseProxy(mustParseURL(config.IncidentsHost, config.IncidentsPort))
+	//gisProxy := httputil.NewSingleHostReverseProxy(mustParseURL(config.GisHost, config.GisPort))
+	navigationProxy := httputil.NewSingleHostReverseProxy(mustParseURL(config.NavigationHost, config.NavigationPort))
 
-	navigationProxy.Rewrite = func(pr *httputil.ProxyRequest) {
-		pr.SetURL(target)
-		pr.Out.URL.Path = "/ws"
-	}
-
-	http.HandleFunc("/navigation/ws", func(w http.ResponseWriter, r *http.Request) {
-		navigationProxy.ServeHTTP(w, r)
-	})
+	http.HandleFunc("/navigation/ws", proxyRedirect(navigationProxy, "/ws"))
 
 	log.Println("Gateway running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
+
+// proxyRedirect prend un proxy et une route de réécriture optionnelle
+func proxyRedirect(proxy *httputil.ReverseProxy, rewriteTo string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if rewriteTo != "" { // Si une réécriture est demandée, on modifie le chemin
+			r.URL.Path = rewriteTo
+		}
+
+		proxy.ServeHTTP(w, r) // Passer la requête au proxy
+	}
+}
+
+func proxyHandle(proxy *httputil.ReverseProxy) http.HandlerFunc {
+	return proxyRedirect(proxy, "")
+}
+
+func mustParseURL(host, port string) *url.URL {
+	rawURL := fmt.Sprintf("http://%s:%s", strings.TrimSpace(host), strings.TrimSpace(port))
+	parsed, err := url.Parse(rawURL)
+	if err != nil {
+		log.Fatalf("Invalid URL %s: %v", rawURL, err)
+	}
+	return parsed
 }
