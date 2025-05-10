ab156b3dbf6e14a79a44ff20b6f42f2aa9930fb4
Ewennn <ewen.bosquet@gmail.com>
2025-05-10 14:44:08 +0200

    feat/ Prepare gateway to redirect all routes

---
+++ config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	UsersHost      string `env:"SUPMAP_USERS_HOST"`
+	UsersPort      string `env:"SUPMAP_USERS_PORT"`
+	IncidentsHost  string `env:"SUPMAP_INCIDENTS_HOST"`
+	IncidentsPort  string `env:"SUPMAP_INCIDENTS_PORT"`
+	GisHost        string `env:"SUPMAP_GIS_HOST"`
+	GisPort        string `env:"SUPMAP_GIST_PORT"`
+	NavigationHost string `env:"SUPMAP_NAVIGATION_HOST"`
+	NavigationPort string `env:"SUPMAP_NAVIGATION_PORT"`
+}
+
+func NewConfig() (*Config, error) {
+	cfg, err := env.ParseAs[Config]()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+	return &cfg, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module supmap-gateway
 
 go 1.24.2
 
-require github.com/coder/websocket v1.8.13 // indirect
+require (
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
+	github.com/coder/websocket v1.8.13 // indirect
+)
